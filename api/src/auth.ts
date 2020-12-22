5e61afc5dcb0d2636d840b74554c3e0b219afb39
harishm72 <harishchowdary72@gmail.com>
2020-12-22 21:41:34 +0530

    api: add util for logIn check

---
+++ api/src/auth.ts
@@ -0,0 +1,8 @@
+import { Request } from "express";
+
+export const logIn = (req: Request, userId: string) => {
+  req.session!.userId = userId;
+};
+
+
+export const isLoggedIn = (req: Request) => !!req.session!.userId
