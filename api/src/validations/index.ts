da1503c0f44614df35df160b8086268e59262009
harishm72 <harishchowdary72@gmail.com>
2020-12-24 00:22:56 +0530

    add loginSchema

--- api/src/validations/index.ts
+++ api/src/validations/index.ts
@@ -1,8 +1,26 @@
 import Joi from "@hapi/joi";
 
+import { BCRYPT_MAX_BYTES } from "../config";
+
+const email = Joi.string()
+  .email()
+  .min(8)
+  .max(254)
+  .lowercase()
+  .trim()
+  .required();
+const name = Joi.string().min(3).max(32).trim().required();
+const password = Joi.string().min(8).max(BCRYPT_MAX_BYTES, "utf8").required();
+const confirmPassword = Joi.valid(Joi.ref("password")).required();
+
 export const registerSchema = Joi.object({
-    email: Joi.string().email().min(8).max(254).lowercase().trim().required(),
-    name: Joi.string().min(3).max(32).trim().required(),
-    password: Joi.string().min(8).max(32).required(),
-    confirmPassword: Joi.valid(Joi.ref('password')).required()
-})
+  email,
+  name,
+  password,
+  confirmPassword,
+});
+
+export const loginSchema = Joi.object({
+  email,
+  password,
+});
