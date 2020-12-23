7460f184cc571279de9edfb0865ce29370011d8c
harishm72 <harishchowdary72@gmail.com>
2020-12-24 00:27:42 +0530

    add preSave hook and utils to User Model

--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -1,17 +1,37 @@
 import { model, Schema, Document } from "mongoose";
+import { compare, hash } from "bcryptjs";
+import { BCRYPT_WORK_FACTOR } from "../config";
 
 interface UserDocument extends Document {
-    email: String
-    name: String
-    password: String
+  email: string;
+  name: string;
+  password: string;
+  matchesPassword: (password: string) => Promise<boolean>;
 }
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     email: String,
     name: String,
     password: String,
-}, {
-    timestamps: true
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre<UserDocument>("save", async function () {
+  if (this.isModified("password")) {
+    this.password = await hash(this.password, BCRYPT_WORK_FACTOR);
+  }
+});
+
+userSchema.methods.matchesPassword = function (password: string) {
+  return compare(password, this.password);
+};
+
+userSchema.set("toJSON", {
+  transform: (doc, { __v, password, ...rest }, options) => rest,
 });
 
 export const User = model<UserDocument>("User", userSchema);
