664c0e9508cb822bdcbf601059806203c9a2f94c
harishm72 <harishchowdary72@gmail.com>
2021-01-26 22:34:13 +0530

    update auth with active to verify Idle timeout

--- api/src/auth.ts
+++ api/src/auth.ts
@@ -1,8 +1,10 @@
-import { Request, Response } from "express";
-import { SESSION_NAME } from "./config";
+import { NextFunction, Request, Response } from "express";
+import { SESSION_ABSOLUTE_TIMEOUT, SESSION_NAME } from "./config";
+import { UnAuthorized } from "./errors";
 
 export const logIn = (req: Request, userId: string) => {
   req.session!.userId = userId;
+  req.session!.createdAt = Date.now();
 };
 
 export const logOut = (req: Request, res: Response) =>
@@ -14,5 +16,24 @@ export const logOut = (req: Request, res: Response) =>
       resolve();
     });
   });
+
+export const active = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (isLoggedIn(req)) {
+    const now = Date.now();
+    const { createdAt } = req.session as Express.Session;
+
+    if (now > createdAt + SESSION_ABSOLUTE_TIMEOUT) {
+      await logOut(req, res);
+
+      return next(new UnAuthorized("Session Expired"));
+    }
+  }
+
+  next();
+};
 
 export const isLoggedIn = (req: Request) => !!req.session!.userId;
