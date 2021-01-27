9b9f0a5d9ec7f3b8ee081faf77fcc3451a3d301a
harishm72 <harishchowdary72@gmail.com>
2021-01-27 23:34:10 +0530

    create apolloClinet and setup provider

--- web/src/components/router/Protected.js
+++ web/src/components/router/Protected.js
@@ -1,15 +1,35 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
+
 import Home from "../Home";
 import Posts from "../Posts";
 
+const createApolloClient = (id) => {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: "http://localhost:8080/v1/graphql",
+      credentials: "include",
+    }),
+    cache: new InMemoryCache(),
+  });
+};
+
+const client = createApolloClient();
+
 const Protected = (props) => {
   return (
-    <>
+    <ApolloProvider client={client}>
+      <header>{props.currentUser.fullName}</header>
       <Route exact path="/posts" component={Posts} />
-      <Route exact path="/home" component={() => <Home logout={props.logout} />} />
+      <Route exact path="/home" component={(routeProps) => <Home {...props} {...routeProps} />} />
       <Redirect path="*" to="/home" />
-    </>
+    </ApolloProvider>
   );
 };
 
